f95d677424a013c30e802d4a974eadeb0b86afb1
codeduchy <codeduchy@gmail.com>
2023-08-15 11:47:38 +0100

    frontend setup

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,28 @@
-import React from 'react';
+import './assets/styles/bootstrap.custom.css';
+import './assets/styles/index.css';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import React from 'react';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index={true} path="/" element={<HomeScreen />} />
+      <Route path="/products/:id" element={<ProductScreen />} />
+    </Route>
+  )
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<RouterProvider router={router} />);
+
 reportWebVitals();
